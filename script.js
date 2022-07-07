5bb9704afafd5e3f94f336ca28414e0a9886e218
derpyfoop <mattheweward181@gmail.com>
2022-07-06 20:58:06 -0400

    Added Selectors.

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM cache
+const selectors = document.getElementsByClassName(".selectors");
+const colorPicker = document.getElementById("#colorPicker");
+const selectorBtns = document.querySelectorAll(".selectorBtns");
+const setColorBtn = document.getElementById("#setColorBtn");
+const RainbowBtn = document.getElementById("#RainbowBtn");
+const earserBtn = document.getElementById("#earserBtn");
+const resetBtn = document.getElementById("#resetBtn");
+const gridCrt = document.getElementById("#gridCrt");
+
