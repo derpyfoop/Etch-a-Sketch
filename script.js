52fe353deccc254e995b61c04b6af76c52bc842a
derpyfoop <mattheweward181@gmail.com>
2022-07-07 00:31:32 -0400

    Added buttons and grid.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // DOM cache
-const selectors = document.getElementsByClassName(".selectors");
+const selectors = document.querySelector(".selectors");
 const colorPicker = document.getElementById("#colorPicker");
 const selectorBtns = document.querySelectorAll(".selectorBtns");
 const setColorBtn = document.getElementById("#setColorBtn");
 const RainbowBtn = document.getElementById("#RainbowBtn");
 const earserBtn = document.getElementById("#earserBtn");
 const resetBtn = document.getElementById("#resetBtn");
-const gridCrt = document.getElementById("#gridCrt");
+const gridCrt = document.querySelector(".gridCrt");
 
+function changeSize(input) {
+    populateGrid(input)
+}
+
+function populateGrid(size) {
+    gridCrt.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    gridCrt.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "blue";
+        gridCrt.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateGrid(16);
